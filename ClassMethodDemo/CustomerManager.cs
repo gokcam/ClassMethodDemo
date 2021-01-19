aca77365d263545a0daf72dd0b816099048a0636
gokcam <gokhancam55@gmail.com>
2021-01-19 20:33:20 +0300

    Add project files.

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class CustomerManager
+    {
+        public void CustomerAdd(Customer customer)
+        {
+            Console.WriteLine("Customer Adding : " + customer.Name + " " + customer.Surname);
+        }
+
+        public void CustomerDelete(Customer customer)
+        {
+            Console.WriteLine("Customer Deleted : " + customer.Name + " " + customer.Surname);
+        }
+        public void ListCustomer(Customer[] customers)
+        {
+            Console.WriteLine("Customer List");
+            foreach (var Customer in customers)
+            {
+                Console.WriteLine(Customer.Name + " " + Customer.Surname);
+            }
+
+
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Name = "Engin";
+            customer1.Surname = "Demiroğ";
+            customer1.Age = 18;
+
+            Customer customer2 = new Customer();
+            customer2.Name = "Gökhan";
+            customer2.Surname = "Çam";
+            customer2.Age = 25;
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.CustomerAdd(customer1);
+            customerManager.CustomerAdd(customer2);
+            customerManager.CustomerDelete(customer1);
+            customerManager.CustomerDelete(customer2);
+
+            Customer[] customers = new Customer[] { customer1,customer2 };
+            customerManager.ListCustomer(customers);
+
+
+
+
+
+        }
+    }
+}
